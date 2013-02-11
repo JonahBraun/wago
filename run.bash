2564fac4836042f2bfcd0f060bd6ce9b2e5e36b3
Jonah Braun <Jonah@goodcoffee.ca>
2013-02-10 20:51:54 -0800

    updated tests

--- run.bash
+++ run.bash
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+simulate_change() {
+	sleep 10
+	for i in {1..4}; do
+		echo "touching"
+		touch tmp.$(date +%s)
+		sleep 4
+	done
+	rm tmp.*
+}
+
+simulate_change &
+
 go install && wago \
-	-cmd='sleep 3s && echo "done sleeping 3s"' \
-	-daemon='echo "begin daemon" && while true; do echo "more output foo bar $(date)" && sleep 10s; done' \
-	-timer=700 \
+	-cmd='sleep 2s && echo "BUILDCMD"' \
+	-daemon='./test_daemon.bash' \
+	-trigger='Quick' \
+	-pcmd='echo POSTCMD' \
+	#-timer=700 \
 	#-url='http://localhost:80/'
---
+++ test_daemon.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "START daemon" && for i in {1..9}; do
+	sleep 10 && echo -e "BEGIN TheQuickBrownFoxJumpsOverTheLazyDog123456 $(date) EOL"
+done
