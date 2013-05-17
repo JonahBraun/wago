7cc553eb86c2109da4cd621239f950e6d85c46b5
Jonah Braun <Jonah@goodcoffee.ca>
2013-05-16 17:19:37 -0700

    add feature watch recursively

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/howeyc/fsnotify"
 	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
@@ -20,8 +21,8 @@ var (
 	verbose      = flag.Bool("v", false, "Verbose")
 	verboseQuiet = flag.Bool("q", false, "Quiet, only warnings and errors")
 
-	fiddle        = flag.Bool("fiddle", false, "CLI fiddle mode, starts a web server and opens url to watchDir/index.html")
-	watchDir      = flag.String("dir", "", "Directory to watch, defaults to current")
+	fiddle        = flag.Bool("fiddle", false, "CLI fiddle mode, starts a web server and opens url to targetDir/index.html")
+	targetDir      = flag.String("dir", "", "Directory to watch, defaults to current")
 	buildCmd      = flag.String("cmd", "", "Bash command to run on change, Wabo will wait for this command to finish")
 	daemonCmd     = flag.String("daemon", "", "Bash command that starts a daemon, Wago will halt if the daemon exits before the trigger or timer")
 	daemonTrigger = flag.String("trigger", "", "A string the daemon will output that indicates it has started successfuly, Wago will continue on this trigger")
@@ -30,6 +31,7 @@ var (
 	postCmd       = flag.String("pcmd", "", "Bash command to run after the daemon has successfully started")
 	url           = flag.String("url", "", "URL to open")
 	watchRegex    = flag.String("watch", `/\w[\w\.]*": (CREATE|MODIFY)`, "Regex to match watch event, use -v to see all events")
+	recursive = flag.Bool("recursive", true, "Watch directory tree recursively")
 
 	daemon = &Daemon{}
 	cmd    = &Cmd{}
@@ -105,14 +107,14 @@ func main() {
 		}
 	}
 
-	if *watchDir == "" {
+	if *targetDir == "" {
 		cwd, err := os.Getwd()
 		if err != nil {
 			panic(err)
 		}
-		watchDir = &cwd
+		targetDir = &cwd
 	}
-	Talk("Watching dir:", *watchDir)
+	Talk("Target dir:", *targetDir)
 
 	r, err := regexp.Compile(*watchRegex)
 	if err != nil {
@@ -125,15 +127,42 @@ func main() {
 	}
 	defer watcher.Close()
 
-	err = watcher.Watch(*watchDir)
-	if err != nil {
-		panic(err)
+	watchDir := func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
+
+		Talk("Watching dir:", path)
+
+		if err !=nil {
+			Err("Skipping dir:", path, err)
+			return filepath.SkipDir
+		}
+
+		err = watcher.Watch(path)
+		if err != nil {
+			panic(err)
+		}
+
+		return nil
+	}
+
+	if *recursive == true {
+		err = filepath.Walk(*targetDir, watchDir)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		err = watcher.Watch(*targetDir)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	if *webServer != "" {
 		go func() {
 			Note("Starting web server on port", *webServer)
-			err := http.ListenAndServe(*webServer, http.FileServer(http.Dir(*watchDir)))
+			err := http.ListenAndServe(*webServer, http.FileServer(http.Dir(*targetDir)))
 			if err != nil {
 				Fatal("Error starting web server:", err)
 			}
